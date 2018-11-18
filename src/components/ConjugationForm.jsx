a0ff5f9c5c519bd1e453a0f8cf2415eb1741877b
Matt Leick <mattleick@Matt-Leicks-MacBook-Pro.local>
2018-11-18 13:59:55 -0600

    there's an idea here that is working
    flesh out basic component structure

--- src/components/ConjugationForm.jsx
+++ src/components/ConjugationForm.jsx
@@ -1,7 +1,105 @@
 import React, { Component } from 'react';
 
+
 export default class ConjugationForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            yo: '',
+            yoCorrect: false,
+            tu: '',
+            tuCorrect: false,
+            ud: '',
+            udsCorrect: false,
+            nos: '',
+            nosCorrect: false,
+            vos: '',
+            vosCorrect: false,
+            uds: '',
+            udsCorrect: false,
+            correctTimeout: null
+        };
+
+        this._inputChange = this.inputChange.bind(this);
+        this._correct = this.correct.bind(this);
+    }
+
+    correct(person) {
+        const { verb } = this.props;
+        this.setState(prevState => (
+            {
+                [`${person}Correct`]: prevState[person] == verb.conjugation[person]
+            }
+        ));
+    }
+
+    inputChange(e) {
+        const pers = e.target.getAttribute('name');
+        clearTimeout(this.state.correctTimeout);
+        const value = e.target.value;
+        this.setState(prevState => (
+            {
+                [pers]: value,
+                correctTimeout: setTimeout(() => this._correct(pers), 500)
+            }
+        ));
+    }
+
     render() {
-        return <p>FORM!</p>
+        const { tense, verb } = this.props;
+        const {
+            yo,
+            yoCorrect,
+            tu,
+            tuCorrect,
+            ud,
+            udCorrect,
+            nos,
+            nosCorrect,
+            vos,
+            vosCorrect,
+            uds,
+            udsCorrect
+        } = this.state;
+        return (
+            <div>
+                <div>{ tense }</div>
+                <div>
+                    <div>
+                        <label>yo</label>
+                        <input 
+                            name='yo'
+                            style={{
+                                border: '1px solid',
+                                borderColor: yoCorrect ? 'green' : 'gray'
+                            }}
+                            onChange={ this._inputChange }
+                            value={ yo }
+                        />
+                    </div>
+                    <div>
+                        <label>tu</label>
+                        <input name='tu' />
+                    </div>
+                    <div>
+                        <label>el/ellas/Ud</label>
+                        <input name='ud' />
+                    </div>
+                    <div>
+                        <label>nos.</label>
+                        <input name='nos' />
+                    </div>
+                    <div>
+                        <label>vos</label>
+                        <input name='vos' />
+                    </div>
+                    <div>
+                        <label>Uds.</label>
+                        <input name='uds' />
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
--- src/components/VerbContainer.jsx
+++ src/components/VerbContainer.jsx
@@ -1,12 +1,170 @@
 import React, { Component } from 'react';
 
 import ConjugationForm from './ConjugationForm';
+import { runInThisContext } from 'vm';
 
 
 export default class VerbContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            verbs: [],
+            nextVerbs: [],
+            idx: 0,
+            tenses: {
+                'present': true,
+                'imperative': true,
+                'preterit': true
+            },
+            hasLoaded: false
+        }
+
+        this._loadVerbs = this.loadVerbs.bind(this);
+        this._toggleTense = this.toggleTense.bind(this);
+        this._nextVerb = this.nextVerb.bind(this);
+    }
+
+    loadVerbs() {
+        // TODO
+        this.setState({
+            hasLoaded: true,
+            verbs: [
+                {
+                    infinitive: 'abrir',
+                    tenses: [
+                        {
+                            englishName: 'present',
+                            conjugation: {
+                                'yo': 'abro',
+                                'tu': 'abras',
+                                'ud': 'abra',
+                                'nos': 'abramos',
+                                'vos': 'abrais',
+                                'uds': 'abran',
+                            }
+                        },
+                        {
+                            englishName: 'present',
+                            conjugation: {
+                                'yo': 'abro',
+                                'tu': 'abras',
+                                'ud': 'abra',
+                                'nos': 'abramos',
+                                'vos': 'abrais',
+                                'uds': 'abran',
+                            }
+                        },
+                        {
+                            englishName: 'present',
+                            conjugation: {
+                                'yo': 'abro',
+                                'tu': 'abras',
+                                'ud': 'abra',
+                                'nos': 'abramos',
+                                'vos': 'abrais',
+                                'uds': 'abran',
+                            }
+                        }
+                    ]
+                },
+                {
+                    infinitive: 'comer',
+                    tenses: [
+                        {},
+                        {},
+                        {}
+                    ]
+                },
+                {
+                    infinitive: "tomar",
+                    tenses: [
+                        {},
+                        {},
+                        {}
+                    ]
+                }
+            ]
+        });
+    }
+
+    componentDidMount() {
+        this._loadVerbs();
+    }
+
+    nextVerb() {
+        this.setState((prevState) => {
+            const newIdx = prevState.idx + 1 > 2
+                ? 0
+                : prevState.idx + 1;
+            return {
+                idx: newIdx
+            };
+        });
+    }
+
+    toggleTense(e) {
+        const name = e.target.getAttribute('name');
+        this.setState(prevState => (
+           {
+                tenses: {
+                    ...prevState.tenses,
+                    [name]: !prevState.tenses[name]
+                }
+           }
+        ));
+    }
+
     render() {
+        const { hasLoaded, tenses, verbs, idx } = this.state;
+        const verb = verbs[idx];
+        const activeTenses = [];
+        Object.keys(tenses).forEach(tk => {
+            if (tenses[tk]) {
+                activeTenses.push(tk);
+            }
+        })
+
         return (
-            <ConjugationForm />
-        )
+            <>
+            {
+                !hasLoaded ? 
+                <h1>LOADING VERBS!</h1> :
+                <div>
+                    <div>
+                        <p 
+                            name='present'
+                            style={{
+                                background: tenses['present'] ? 'lightblue' : 'white'
+                            }} 
+                            onClick={ this._toggleTense }>Present</p>
+                        <p 
+                            name='imperative'
+                            style={{
+                                background: tenses['imperative'] ? 'lightblue' : 'white'
+                            }} 
+                            onClick={ this._toggleTense }>Imperative</p>
+                        <p 
+                            name='preterit'
+                            style={{
+                                background: tenses['preterit'] ? 'lightblue' : 'white'
+                            }} 
+                            onClick={ this._toggleTense }>Preterit</p>
+                    </div>
+                    <h1>{ verb.infinitive }</h1>
+                    <p onClick={ this._nextVerb }>next >></p>
+                    {
+                        activeTenses.map(tense => (
+                            <ConjugationForm 
+                                key={ tense }
+                                tense={ tense } 
+                                verb={ verbs[idx].tenses.find(v => v.englishName == tense) } 
+                            />
+                        ))
+                    }
+                </div>
+            }
+            </>
+        );
     }
 }
